d36382cd9074e4c9c5b4b9672f7a0da996ff8d94
Murilo Damario <murilo.gcdamario@gmail.com>
2026-01-05 22:34:31 -0300

    :sparkles: feat: Configure plugins

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,40 @@
 import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiRefrence from '@scalar/fastify-api-reference'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(ScalarApiRefrence, {
+  routePrefix: '/docs'
+})
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running! 🚀')
+  console.log('🚀 HTTP server running on http://localhost:3333!')
+  console.log('📚 API docs available on http://localhost:3333/docs')
 })
