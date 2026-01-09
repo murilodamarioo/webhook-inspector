9ae40e43e5a39fc1e704f7589666d9dca9c2b2ef
Murilo Damario <murilo.gcdamario@gmail.com>
2026-01-09 20:27:53 -0300

    :wrench: chore: Tailwindcss and Tanstack setup

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true
+    }),
+  ],
 })
+
