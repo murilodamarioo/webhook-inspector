0006a51bd2c33123f216407ef7ceab636d8401ae
Murilo Damario <murilo.gcdamario@gmail.com>
2026-01-10 16:26:51 -0300

    :sparkles: feat: Create Sidebar and CopyButton components

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+
+import { Sidebar } from '../components/sidebar'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -6,8 +9,18 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className='h-screen bg-zinc-900'>
+      <PanelGroup direction='horizontal'>
+        <Panel defaultSize={20} minSize={15} maxSize={40}>
+          <Sidebar />
+        </Panel>
+
+        <PanelResizeHandle className='w-px bg-zinc-700 hover:bg-zinc-600 transition-colors duration-150' />
+
+        <Panel defaultSize={80} minSize={60}>
+          Content
+        </Panel>
+      </PanelGroup>
     </div>
   )
 }
